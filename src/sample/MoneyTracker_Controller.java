aae6e35b59363987f3befc1275091fdd961e1454
kh-abrar <khandaker.abrar19@gmail.com>
2021-10-01 19:31:08 +0600

    Add-Transaction Controller file added

--- src/sample/MoneyTracker_Controller.java
+++ src/sample/MoneyTracker_Controller.java
@@ -3,15 +3,19 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MoneyTracker_Controller {
+public class MoneyTracker_Controller{
     @FXML
     TextArea area;
     @FXML
