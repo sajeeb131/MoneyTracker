2603426abd7d20c00a1393ba2c7afb8a98b670ea
Yeasin Hasan <yeasinhasan9822@gmail.com>
2021-10-08 20:09:47 +0600

    Main Controller updated

--- src/sample/MoneyTracker_Controller.java
+++ src/sample/MoneyTracker_Controller.java
@@ -8,16 +8,42 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MoneyTracker_Controller{
     @FXML
     TextArea area;
+
+    @FXML
+    Label usernameLabel, nameL,balanceL,loanL,currencyL;
+    String username,name,balance,loan,currency;
+
+    static BufferedWriter writer=logInPage_Controller.getWriter();
+    static BufferedReader reader=logInPage_Controller.getReader();
+
+    public MoneyTracker_Controller() throws IOException{
+        username=reader.readLine();
+        name=reader.readLine();
+        balance=reader.readLine();
+        loan=reader.readLine();
+        currency=reader.readLine();
+    }
+    @FXML
+    public void initialize() {
+        usernameLabel.setText(username);
+        nameL.setText(name);
+        balanceL.setText(balance);
+        loanL.setText(loan);
+        currencyL.setText(currency);
+    }
     @FXML
     private void addButton(ActionEvent event){
         try {
@@ -33,21 +59,5 @@ public class MoneyTracker_Controller{
             e.printStackTrace();
         }
 
-    }
-
-
-    public void currencyConverterButton(ActionEvent event) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("FXML/CurrencyConverter.fxml"));
-            Scene scene=new Scene(root);
-            Stage window = new Stage();
-            window.setTitle("Currency Converter");
-            window.setScene(scene);
-            window.setAlwaysOnTop(true);
-            window.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
